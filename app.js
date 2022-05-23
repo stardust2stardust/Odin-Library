6b9665471467c80f19de3427004e9dcb6e0c87fa
Jeni Hoyt <ziahrahl@gmail.com>
2022-05-23 16:47:31 -0400

    add function to loop over library and create new cards on page

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 let myLibrary = [];
 
+const addBookBtn = document.querySelector('.add-book-btn')
+const form = document.querySelector('form')
+const submit = document.querySelector('.submit-btn>button')
+const titleInput = document.querySelector('#title')
+const authorInput = document.querySelector('#author')
+const pagesinput = document.querySelector('#pages')
+const hasReadInput = document.querySelectorAll('input[name="read"]')
+const main = document.querySelector('main')
+
+
 function Book(title, author, pages, hasRead) {
     this.title = title
     this.author = author
@@ -11,14 +21,52 @@ function Book(title, author, pages, hasRead) {
     }
 }
 
+function submitForm(e) {
+    e.preventDefault();
+    addBookToLibrary();
+}
+
 function addBookToLibrary() {
-    let title = prompt("Title: ");
-    let author = prompt("Author: ")
-    let pages = prompt("Pages: ")
-    let hasRead = prompt("Read it? ")
+    let title = titleInput.value
+    let author = authorInput.value
+    let pages = pagesinput.value
+    let hasRead = hasReadInput.value
 
     const newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook)
+
+    loopOverBooks();
+}
+
+function showForm() {
+    form.classList.remove('show-hide')
+}
+
+function loopOverBooks(listOfBooks) {
+    listOfBooks = myLibrary
+    myLibrary.forEach(element => {
+        console.log(element);
+        createNewCard();
+    });
+}
+function createNewCard() {
+    let card = document.createElement('div');
+
+    let h2 = document.createElement('h2')
+    let pAuthor = document.createElement('p')
+    let pPages = document.createElement('p')
+    let pRead = document.createElement('p')
+    h2.innerText = "New Book Title"
+    pAuthor.innerText = "author goes here"
+    pPages.innerText = "pages goes here"
+    pRead.innerText = "yes or no"
+    main.appendChild(card)
+    card.appendChild(h2)
+    card.appendChild(pAuthor)
+    card.append(pPages)
+    card.append(pRead)
+
+    card.classList.add('card');
 }
 
 // addBookToLibrary();
@@ -28,7 +76,8 @@ const book02 = new Book("Stone of Tears", "Terry Goodkind", 979, true)
 myLibrary.push(book01)
 myLibrary.push(book02)
 
-
+addBookBtn.addEventListener('click', showForm)
+submit.addEventListener('click', submitForm)
 
 
 
