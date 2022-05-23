5fcd7349c7e8a05a85093c57763509261bc98ae8
Jeni Hoyt <ziahrahl@gmail.com>
2022-05-23 13:24:26 -0400

    create basic layout and styling for library page

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+let myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.hasRead = hasRead
+    this.info = function () {
+        const bookInfo = `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasRead}`;
+        return bookInfo;
+    }
+}
+
+function addBookToLibrary() {
+    let title = prompt("Title: ");
+    let author = prompt("Author: ")
+    let pages = prompt("Pages: ")
+    let hasRead = prompt("Read it? ")
+
+    const newBook = new Book(title, author, pages, hasRead);
+    myLibrary.push(newBook)
+}
+
+// addBookToLibrary();
+
+const book01 = new Book("Wizard's First Rule", "Terry Goodkind", 836, true);
+const book02 = new Book("Stone of Tears", "Terry Goodkind", 979, true)
+myLibrary.push(book01)
+myLibrary.push(book02)
+
+
 
