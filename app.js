c3f0ecc050568ec86ae1f998ed91fc745667859c
Jeni Hoyt <ziahrahl@gmail.com>
2022-05-25 17:18:13 -0400

    replace svg w/ png, hide form when cancel button is clicked

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let myLibrary = [];
 
+
 const addBookBtn = document.querySelector('.add-book-btn')
+const delBookBtn = document.querySelectorAll('.del-book-btn')
 
 // Constructor //
 function Book(title, author, pages, hasRead, rating) {
@@ -9,12 +11,14 @@ function Book(title, author, pages, hasRead, rating) {
     this.pages = pages
     this.hasRead = hasRead
     this.rating = rating
+
 }
 
 
 function showForm() {
     const form = document.querySelector('form')
     const submit = document.querySelector('.submit-btn')
+
     addBookBtn.classList.add('show-hide')
     form.classList.remove('show-hide')
     submit.addEventListener('click', submitForm);
@@ -24,23 +28,30 @@ function showForm() {
     ratingOutput.innerText = ratingInput.value
     ratingInput.addEventListener('input', () => {
         ratingOutput.innerText = ratingInput.value
+    });
+    const cancel = document.querySelector('.cancel-btn')
+    cancel.addEventListener('click', () => {
+        console
+        form.classList.add('show-hide')
+        addBookBtn.classList.remove('show-hide')
     })
 }
 
 
 function submitForm(e) {
     e.preventDefault();
-
     const titleInput = document.querySelector('#title');
     const authorInput = document.querySelector('#author');
     const pagesInput = document.querySelector('#pages');
     let title = titleInput.value;
     let author = authorInput.value;
     let pages = pagesInput.value;
+
     // check that text inputs were filled out
     if (title.length < 1 || author.length < 1 || pages.length < 1) {
         alert("Please make sure all fields are filled out")
     } else {
+
         addBookToLibrary(title, author, pages);
         const form = document.querySelector('form')
         addBookBtn.classList.remove('show-hide');
@@ -59,23 +70,18 @@ function addBookToLibrary(title, author, pages) {
 }
 
 
-
-// function loopOverBooks(listOfBooks) {
-//     listOfBooks = myLibrary
-//     myLibrary.forEach(element => {
-//         console.log(element);
-//         createNewCard(element);
-//     });
-// }
-
-
 function createNewCard(newBook) {
     let card = document.createElement('div');
     let h2 = document.createElement('h2');
+    let bookInfo = document.createElement('div')
     let pAuthor = document.createElement('p');
     let pPages = document.createElement('p');
     let pRead = document.createElement('p');
     let pRating = document.createElement('p');
+    let update = document.createElement('div')
+    let imgRead = document.createElement('img');
+    let imgRemove = document.createElement('img');
+
     h2.innerText = newBook.title;
     pAuthor.innerText = newBook.author;
     pPages.innerText = `${newBook.pages} pages`;
@@ -86,17 +92,32 @@ function createNewCard(newBook) {
         pRead.innerText = 'Not read yet';
         pRating.innerText = `n/a`;
     }
+
+    imgRead.src = "/images/check-bold.png"
+    imgRemove.src = "/images/close-thick.png"
     const main = document.querySelector('main')
 
-    main.appendChild(card);
-    card.appendChild(h2);
-    card.appendChild(pAuthor);
-    card.append(pPages);
-    card.append(pRead);
-    card.append(pRating);
-    card.classList.add('card');
+    main.appendChild(card).classList.add('card');
+    card.appendChild(h2).classList.add('book-title');
+    card.appendChild(bookInfo).classList.add('book-info')
+    card.appendChild(update).classList.add('update')
+    bookInfo.appendChild(pAuthor).classList.add('info-p');
+    bookInfo.appendChild(pPages).classList.add('info-p');
+    bookInfo.appendChild(pRead).classList.add('info-p');
+    bookInfo.appendChild(pRating).classList.add('info-p');
+    if (newBook.hasRead === 'true') {
+        update.appendChild(imgRead).classList.add('show-hide');
+    } else {
+        update.appendChild(imgRead).classList.add('update>img');
+    };
+    update.appendChild(imgRemove).classList.add('update>img');
+    assignNumsToCards();
 }
 
+function removeBook(e) {
+    console.log('This will remove book from the library');
+    console.log(e.node)
+}
 
 
 const book01 = new Book("Wizard's First Rule", "Terry Goodkind", 836, true, 10);
@@ -105,6 +126,18 @@ myLibrary.push(book01)
 myLibrary.push(book02)
 
 addBookBtn.addEventListener('click', showForm)
+
+function assignNumsToCards() {
+    const cards = document.querySelectorAll('.card');
+    let i = 0;
+    cards.forEach(card => {
+        let cardIndex = i;
+        i++;
+        console.log(`card #: ${cardIndex}`)
+
+    })
+}
+
 
 
 
